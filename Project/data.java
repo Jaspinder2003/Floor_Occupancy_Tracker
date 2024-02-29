7a5c966904bc5344b53e0aa48cb1422fbc03c73d
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-02-29 21:43:28 +0000

    Added a methods that helps to input key and value inside the HasMap.

--- Project/data.java
+++ Project/data.java
@@ -1,3 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap; //Imports the hashmap class.
+
 public class data {
+    static HashMap<Integer, Integer> floor_occupancy = new HashMap<Integer,Integer>(); //HashMap created for further usage.
     
+    public static HashMap<Integer, Integer> floor_add(HashMap<Integer, Integer> floor_filled, int key, int value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
+        floor_filled.put(key, value);
+        return floor_filled;
+    }
 }
