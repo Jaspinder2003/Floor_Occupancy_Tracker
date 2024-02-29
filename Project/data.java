ee385fad7f1d5bf1c3a3b73f8aea0c561e259053
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-02-29 21:53:11 +0000

    Update floor_filled().

--- Project/data.java
+++ Project/data.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.HashMap; //Imports the hashmap class.
 
 public class data {
-    static HashMap<Integer, Integer> floor_occupancy = new HashMap<Integer,Integer>(); //HashMap created for further usage.
+    static HashMap<Integer, String> floor_occupancy = new HashMap<Integer, String>(); //HashMap created for further usage.
     
-    public static HashMap<Integer, Integer> floor_add(HashMap<Integer, Integer> floor_filled, int key, int value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
+    public static HashMap<Integer, String> floor_add(HashMap<Integer, String> floor_filled, int key, String value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
         floor_filled.put(key, value);
         return floor_filled;
     }
