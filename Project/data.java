1867053e4bb33e9cc17ff446928b2dd338a0e910
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-03-01 15:50:00 +0000

    Did some correction in floor_occupancy and computer_occupancy.

--- Project/data.java
+++ Project/data.java
@@ -11,7 +11,6 @@ public class data {
             floor_vacancy.put("2", 134);
             floor_vacancy.put("3", 200);
     }
-
     //Navpreet Singh
     public static HashMap<String, Integer> computer_vacancy = new HashMap<String, Integer>();//stores and manages availability of computers on each floor.
     static{
@@ -19,22 +18,21 @@ public class data {
         computer_vacancy.put("2", 60);
         computer_vacancy.put("3", 75);
     }
-
     //Yadwinder Singh Dhaliwal
-    public static Integer floor_value_finder(String num){//stores and manages availability on each floor.
+    public static int floor_value_finder(String num){//stores and manages availability on each floor.
         int floor_num = floor_vacancy.get(num);
         return floor_num;
     }
     //Yadwinder Singh Dhaliwal
-    public static Integer computer_value_finder(String num){//stores and manages availability on each floor.
+    public static int computer_value_finder(String num){//stores and manages availability on each floor.
         int computer_num = computer_vacancy.get(num);
         return computer_num;
     }
 
     //Navpreet Singh
-    static HashMap<Integer, String> floor_occupancy = new HashMap<Integer, String>(); //HashMap created for further usage.
+    public static HashMap<Integer, Integer[]> floor_occupancy = new HashMap<Integer, Integer[]>(); //HashMap created for further usage.
     
-    public static HashMap<Integer, String> floor_add(HashMap<Integer, String> floor_filled, int key, String value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
+    public static HashMap<Integer, Integer[]> floor_add(HashMap<Integer, Integer[]> floor_filled, int key, Integer[] value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
         floor_filled.put(key, value);
         return floor_filled;
     }
