de9f24a4234716528e272b69f6f7eda2a7b5c019
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-21 21:30:34 +0000

    changed data

--- Project/data.java
+++ Project/data.java
@@ -1,51 +1,74 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap; //Imports the hashmap class.
 
 public class data {
-    //Jaspinder Singh
+    private int id;
+    private String name;
     public static HashMap<String, Integer> floor_vacancy = new HashMap<String, Integer>();//stores and manages availability on each floor.
-    static{
-            floor_vacancy.put("1", 87);
-            floor_vacancy.put("2", 134);
-            floor_vacancy.put("3", 200);
+
+    public data(String name,int id){
+        this.name=name;
+        this.id=id;
+
     }
-    //Navpreet Singh
+    static {
+        floor_vacancy.put("1", 87);
+        floor_vacancy.put("2", 134);
+        floor_vacancy.put("3", 200);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getID(){
+        return id;
+    }
+    public HashMap<String, Integer> getFloor_vacancy() {
+        return floor_vacancy;
+    }
+    public void set_floor_vacancy(String key,int value){
+        floor_vacancy.put(key,value);
+    }
+
+
     public static HashMap<String, Integer> computer_vacancy = new HashMap<String, Integer>();//stores and manages availability of computers on each floor.
     static{
         computer_vacancy.put("1", 30);
         computer_vacancy.put("2", 60);
         computer_vacancy.put("3", 75);
     }
-    //Yadwinder Singh Dhaliwal
+    public HashMap<String, Integer> getComputer_vacancy() {
+        return floor_vacancy;
+    }
+    public void set_computer_vacancy(String key,int value){
+        computer_vacancy.put(key,value);
+    }
     public static int floor_value_finder(String num){//stores and manages availability on each floor.
         int floor_num = floor_vacancy.get(num);
         return floor_num;
     }
-    //Yadwinder Singh Dhaliwal
+
     public static int computer_value_finder(String num){//stores and manages availability on each floor.
         int computer_num = computer_vacancy.get(num);
         return computer_num;
     }
 
-    //Navpreet Singh
+
     public static HashMap<Integer, Integer[]> floor_occupancy = new HashMap<Integer, Integer[]>(); //HashMap created for further usage.
-    
+
     public static HashMap<Integer, Integer[]> floor_add(HashMap<Integer, Integer[]> floor_filled, int key, Integer[] value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
         floor_filled.put(key, value);
         return floor_filled;
     }
 
-    //Navpreet Singh
     public static HashMap<Integer, Integer[]> computer_occupancy = new HashMap<Integer, Integer[]>(); //HashMap created for further usage.
-    
+
     public static HashMap<Integer, Integer[]> computer_add(HashMap<Integer, Integer[]> computer_filled, int key, Integer[] value){ //method used to add information (key to be studentID, value as the floor and area chosen.)
         computer_filled.put(key, value);
         return computer_filled;
     }
 
-    //Jaspinder Singh Maan
     public static ArrayList<Integer> computer_area_users = new ArrayList<>();
 
     public static ArrayList<Integer> computer_area_add(Integer studentID){
@@ -57,4 +80,6 @@ public class data {
         int count = computer_area_users.size();
         return count;
     }
+
 }
+
