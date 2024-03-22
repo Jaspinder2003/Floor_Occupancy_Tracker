31fd42413c39a1b9e1b0782b41ae7309cf97c1ce
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-22 06:19:54 +0000

    changed data

--- Project/data.java
+++ Project/data.java
@@ -2,19 +2,17 @@ import java.util.ArrayList;
 import java.util.HashMap; //Imports the hashmap class.
 
 public class data {
-    private int id;
+    private Integer id;
     private String name;
     private int floor;
     public static HashMap<Integer, Integer> floor_vacancy = new HashMap<Integer, Integer>();//stores and manages availability on each floor.
 
-    public data(String name,int id,int floor){
+    public data(String name,int id){
         this.name=name;
         this.id=id;
 
     }
-    public int getFloor(){
-        return floor;
-    }
+
     static {
         floor_vacancy.put(1, 87);
         floor_vacancy.put(2, 134);
@@ -25,14 +23,17 @@ public class data {
         return name;
     }
 
-    public int getID(){
+    public Integer getID(){
         return id;
     }
     public HashMap<Integer, Integer> getFloor_vacancy() {
         return floor_vacancy;
     }
-    public void set_floor_vacancy(Integer key,int value){
-        floor_vacancy.put(key,value);
+    protected void set_floor_vacancy(Integer key,int value){
+        if (key == null || value < 0) {
+            throw new IllegalArgumentException("Key cannot be null and value must be non-negative.");
+        }
+        floor_vacancy.put(key,value);//putting the new value in place of the given key
     }
 
 
@@ -45,55 +46,12 @@ public class data {
     public HashMap<Integer, Integer> getComputer_vacancy() {
         return computer_vacancy;
     }
-    public void set_computer_vacancy(Integer key,int value){
-        computer_vacancy.put(key,value);
+    protected void set_computer_vacancy(Integer key,int value){
+        if (key == null || value < 0) {
+            throw new IllegalArgumentException("Key cannot be null and value must be non-negative.");
+        }
+        computer_vacancy.put(key,value);//putting the value for the given key
     }
-    public static int floor_value_finder(int num){//stores and manages availability on each floor.
-        int floor_num = floor_vacancy.get(num);
-        return floor_num;
-    }
-
-    public static int computer_value_finder(int num){//stores and manages availability on each floor.
-        int computer_num = computer_vacancy.get(num);
-        return computer_num;
-    }
-
-
-    public static HashMap<Integer, Integer[]> floor_occupancy = new HashMap<Integer, Integer[]>(); //HashMap created for further usage.
-
-    public static HashMap<Integer, Integer[]> floor_add(HashMap<Integer, Integer[]> floor_filled, int key, Integer[] value){ //method used to add information (key to be studentID, value as the floor-area chosen.)
-        floor_filled.put(key, value);
-        return floor_filled;
-    }
-
-    public static HashMap<Integer, Integer[]> computer_occupancy = new HashMap<Integer, Integer[]>(); //HashMap created for further usage.
-
-    public static HashMap<Integer, Integer[]> computer_add(HashMap<Integer, Integer[]> computer_filled, int key, Integer[] value){ //method used to add information (key to be studentID, value as the floor and area chosen.)
-        computer_filled.put(key, value);
-        return computer_filled;
-    }
-
-    public static ArrayList<Integer> computer_area_users = new ArrayList<>();
-
-    public static ArrayList<Integer> computer_area_add(Integer studentID){
-        computer_area_users.add(studentID);
-        return computer_area_users;
-    }
-
-    public static int computer_area_counter(){
-        int count = computer_area_users.size();
-        return count;
-    }
-
-    public String toString(){
-        
-        return null;
-    }
-
-    private ArrayList<data> info1= new ArrayList<>();
-    private ArrayList<data> info2= new ArrayList<>();
-    private ArrayList<data> info3= new ArrayList<>();
-
 
 }
 
