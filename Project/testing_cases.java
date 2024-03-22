9ec3f174d39d12fd321dcbb3b73575b17f74c515
Yadwinder Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-03-22 16:38:17 +0000

    Update testing_cases.java

--- Project/testing_cases.java
+++ Project/testing_cases.java
@@ -1,14 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
-import org.junit.Test;
 import java.util.ArrayList;
-import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.Collections;
 
 class tests {
@@ -17,55 +11,55 @@ class tests {
 
 @org.junit.jupiter.api.Test
     public void floortest1() {
-    data D1=new data("hi",1234);
-    data D2=new data("hello",12345);
+    data D1=new data("hi",1234, 2);
+    data D2=new data("hello",12345, 2);
     D1.set_floor_vacancy(1,23);
     floor floor=new floor("hi",1234,1);
-    int a = floor.getFloorAvailability(D1);
-    floor.new_flr_ava(24,D1);
-    a=floor.getFloorAvailability(D2);
+    int a = floor.getFloorAvailability(2,D1);
+    floor.new_flr_ava(2,24,D1);
+    a=floor.getFloorAvailability(2,D2);
     assertEquals(24,a);
     }
 
     @org.junit.jupiter.api.Test
     public void floortest2() {
-        data D1=new data("hi",1234);
+        data D1=new data("hi",1234, 1);
         D1.set_floor_vacancy(1,23);
         floor floor=new floor("hi",1234,4);
-        int a = floor.getFloorAvailability(D1);
+        int a = floor.getFloorAvailability(1, D1);
         assertThrows(IllegalArgumentException.class, () -> {
-            floor.getFloorAvailability(D1);
+            floor.getFloorAvailability(1, D1);
         }, "Expected getFloorAvailability to throw IllegalArgumentException for non-existent floor.");
     }
 
     @org.junit.jupiter.api.Test
     public void Comptest1() {
-        data D1=new data("hi",1234);
-        data D2=new data("hello",12345);
+        data D1=new data("hi",1234, 3);
+        data D2=new data("hello",12345, 3);
         D1.set_computer_vacancy(1,23);
         Computers computer=new Computers("hi",1234,1);
-        int a = computer.getComputerAvailability(D1);
-        computer.new_computer_ava(30,D2);
-        a=computer.getComputerAvailability(D1);
+        int a = computer.getComputerAvailability(3, D1);
+        computer.new_computer_ava(3, 30,D2);
+        a=computer.getComputerAvailability(3, D1);
         assertEquals(30,a);
     }
 
     @org.junit.jupiter.api.Test
     public void Comptest2() {
-        data D1=new data("hi",1234);
+        data D1=new data("hi",1234, 2);
         D1.set_computer_vacancy(1,23);
         Computers computer=new Computers("hi",1234,4);
-        int a = computer.getComputerAvailability(D1);
+        int a = computer.getComputerAvailability(2, D1);
         assertThrows(IllegalArgumentException.class, () -> {
-            computer.getComputerAvailability(D1);
+            computer.getComputerAvailability(2, D1);
         }, "Expected getComputer availability to throw IllegalArgumentException for non-existent floor.");
     }
 
     @org.junit.jupiter.api.Test
     public void comparatorTest() {
-        data D1=new data("hi",1);
-        data D2=new data("hello",5);
-        data D3=new data("hey",6);
+        data D1=new data("hi",1,2);
+        data D2=new data("hello",5,2);
+        data D3=new data("hey",6,3);
         ArrayList<data> lst=new ArrayList<>();
         lst.add(D3);
         lst.add(D1);
@@ -78,11 +72,11 @@ class tests {
 
    @org.junit.jupiter.api.Test
     public void gridformatTest() {
-        data D1=new data("hi",1);
-        data D2=new data("hello",3);
-        data D3=new data("hey",4);
-        data D4=new data("hmm",2);
-        data D5=new data("yes!!",5);
+        data D1=new data("hi",1,1);
+        data D2=new data("hello",3,1);
+        data D3=new data("hey",4,1);
+        data D4=new data("hmm",2,1);
+        data D5=new data("yes!!",5,1);
         ArrayList<data> name = new ArrayList<>();
         name.add(D1);
         name.add(D2);
