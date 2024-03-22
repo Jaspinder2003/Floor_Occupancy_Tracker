32cb1138a215e43b2a605315f173e2926499013a
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-03-22 16:03:06 +0000

    updated data file

--- Project/data.java
+++ Project/data.java
@@ -49,4 +49,85 @@ public class data {
             e.printStackTrace();
         }
     }
+    
+
+    public static void reader(String fileName) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            br.readLine(); // Skip header line
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                String elementType = values[0];
+                int id = Integer.parseInt(values[1]);
+                String name = values[2];
+                int floor = Integer.parseInt(values[3]);
+
+                switch (elementType) {
+                    case "Data":
+                     data userData = new data(name, id, floor);
+                     data.AddUser(userData);
+                        break;
+                    case "Floor":
+                        floor userFloor = new floor(name, id, floor);
+                        floor.AddUser(userFloor);
+
+                        break;
+                    case "Computers":
+                        Computers userComputer = new Computers(name, id, floor);
+                        Computers.AddUser(userComputer);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    static {
+        floor_vacancy.put(1, 87);
+        floor_vacancy.put(2, 134);
+        floor_vacancy.put(3, 200);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getfloor(){
+        return floor;
+    }
+
+    public Integer getID(){
+        return id;
+    }
+    public HashMap<Integer, Integer> getFloor_vacancy() {
+        return floor_vacancy;
+    }
+    protected void set_floor_vacancy(Integer key,int value){
+        if (key == null || value < 0) {
+            throw new IllegalArgumentException("Key cannot be null and value must be non-negative.");
+        }
+        floor_vacancy.put(key,value);//putting the new value in place of the given key
+    }
+
+
+    public static HashMap<Integer, Integer> computer_vacancy = new HashMap<Integer, Integer>();//stores and manages availability of computers on each floor.
+    static{
+        computer_vacancy.put(1, 30);
+        computer_vacancy.put(2, 60);
+        computer_vacancy.put(3, 75);
+    }
+    public HashMap<Integer, Integer> getComputer_vacancy() {
+        return computer_vacancy;
+    }
+    protected void set_computer_vacancy(Integer key,int value){
+        if (key == null || value < 0) {
+            throw new IllegalArgumentException("Key cannot be null and value must be non-negative.");
+        }
+        computer_vacancy.put(key,value);//putting the value for the given key
+    }
+
 }
+
