9c12bbb76a3167a250afcf294f795b4b38d941c8
Yadwinder Singh Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-03-21 19:54:16 -0600

    Updated file name.

--- Project/main.java
+++ Project/main.java
@@ -1,3 +1,17 @@
-public class main {
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to TFDL!\nSpaces free\n" + stats.show_vacancy("all floors"));
+        System.out.println("Do your want to:\n1. Check In\n2. Check Out\n3. Statistics");
+
+        int choice = input.nextInt();
+
+    }
+
+    public static void SignIn() {
+
+    }
 }
