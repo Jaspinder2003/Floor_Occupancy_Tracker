92aae3d733d833ff77e81688cf020d5ca03d10b2
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-03-22 09:11:53 +0000

    added hashset to store objects and get floor method

--- Project/Computers.java
+++ Project/Computers.java
@@ -1,31 +1,40 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Computers extends data{
     private int computers_availability;
     private String name;
     private int id;
     private int floor;
+
+    private static HashSet<Computers> ComputerObjects = new HashSet<Computers>(); // Set to store reported fighter spaceships
+
     public Computers(String name,int id,int floor){
         super(name,id);
         this.name=name;
         this.id=id;
         this.floor=floor;
+
     }
 
-    public int getComputerAvailability(data data){
-        HashMap<Integer,Integer> m =data.getComputer_vacancy();
-        if (!m.containsKey(this.floor)) {
+    public Integer getFloor(){
+        return floor;
+    }
+
+    public static void AddUser(Computers computers) {
+        ComputerObjects.add(computers);
+    }
+
+    public int getComputerAvailability(Integer floor,data data){
+        HashMap<Integer,Integer> m =data.getFloor_vacancy();
+        if (!m.containsKey(floor)) {
             throw new IllegalArgumentException("Floor does not exist.");
         }
-        this.computers_availability=m.get(this.floor);
+        this.computers_availability=m.get(floor);
         return computers_availability;
     }
-    public void new_computer_ava(int computer_ava,data data){
-        data.set_computer_vacancy(this.floor,computer_ava);
+    public void new_computer_ava(Integer floor,int computer_ava,data data){
+        data.set_computer_vacancy(floor,computer_ava);
     }
-
-
-
 
 }
