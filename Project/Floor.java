2aa7fc673f941c1c0944053d2ffac6c018afe9aa
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-22 06:10:19 +0000

    changed floor

--- Project/Floor.java
+++ Project/Floor.java
@@ -6,17 +6,20 @@ class floor extends data{
     private int id;
     private int floor;
     public floor(String name,int id,int floor){
-        super(name,id,floor);
+        super(name,id);
         this.floor=floor;
     }
 
-    public int getFloorAvailability(Integer floor,data data){
+    public int getFloorAvailability(data data){
         HashMap<Integer,Integer> m =data.getFloor_vacancy();
-        this.floor_availability=m.get(floor);
+        if (!m.containsKey(floor)) {
+            throw new IllegalArgumentException("Floor does not exist.");
+        }
+        this.floor_availability=m.get(this.floor);
         return floor_availability;
     }
-    public void new_flr_ava(Integer floor,int flr_ava,data data){
-        data.set_floor_vacancy(floor,flr_ava);
+    public void new_flr_ava(int flr_ava,data data){
+        data.set_floor_vacancy(this.floor,flr_ava);
     }
 
 
