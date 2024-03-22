07d3463a7f1b3ab3a0f5c67bf066e60021e5d433
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-22 06:22:41 +0000

    Upload New File

---
+++ Project/dataidcomparitor.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+class dataidcomparator implements Comparator<data> {
+    public int compare(data D1 ,data D2){
+        return Integer.compare(D1.getID(),D2.getID());
+    }
+}
