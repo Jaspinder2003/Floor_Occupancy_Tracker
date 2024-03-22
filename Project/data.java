b5ba96177a41d239ba4a84e93dffbb12915b80b6
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-03-22 09:09:35 +0000

    added file writer and reader function along with a hashset for storing objects

--- Project/data.java
+++ Project/data.java
@@ -1,17 +1,85 @@
 import java.util.ArrayList;
 import java.util.HashMap; //Imports the hashmap class.
+import java.util.HashSet;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class data {
     private Integer id;
     private String name;
     private int floor;
+ 
     public static HashMap<Integer, Integer> floor_vacancy = new HashMap<Integer, Integer>();//stores and manages availability on each floor.
+    
+ 
+    private static HashSet<data> dataObjects = new HashSet<data>(); // Set to store objects
+
 
     public data(String name,int id){
         this.name=name;
         this.id=id;
 
     }
+
+    public static void AddUser(data data) {
+        dataObjects.add(data);
+    }
+
+    public static void writer(HashSet<Object> set, String fileName, String ElementType) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            // Write headers
+            writer.write("ID,Name,Floor\n");
+            for (Object elements : set) {
+                if (elements instanceof data) { // data and its subclasses have ID and Name
+                    data d = (data) elements;
+                    String line = ElementType + "," + d.getID() + "," + d.getName();
+                    writer.write(line + "\n");}
+                    // Check for floor attribute
+                else if (elements instanceof floor) { // If it's an instance of floor, it has floor information
+                    floor f = (floor) elements;
+                    String line = ElementType + "," + f.getID() + "," + f.getName()+ "," + f.getFloor();
+                    writer.write(line + "\n"); } 
+                else if (elements instanceof Computers) { // Similarly for Computers
+                    Computers C = (Computers) elements;
+                   String  line = ElementType + "," + C.getID() + "," + C.getName()+ "," + C.getFloor();
+                   writer.write(line + "\n"); } 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reader(String fileName) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            br.readLine(); // Skip header line
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                String elementType = values[0];
+                int id = Integer.parseInt(values[1]);
+                String name = values[2];
+                int floor = Integer.parseInt(values[3]);
+
+                switch (elementType) {
+                    case "Data":
+                     //   dataObjects.add(new data(name, id));
+                        break;
+                    case "Floor":
+                     // floorObjects.add(new floor(name, id, floor));
+                        break;
+                    case "Computers":
+                      // ComputerObjects.add(new Computers(name, id, floor));
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
 
     static {
         floor_vacancy.put(1, 87);
@@ -54,4 +122,3 @@ public class data {
     }
 
 }
-
