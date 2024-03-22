47f69febcbcf81423b57170de3f32d008c27553e
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-22 06:02:53 +0000

    added one more test case

--- Project/testing_cases.java
+++ Project/testing_cases.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import java.util.Collections;
 
 class tests {
-//jaspinder singh maan
+
 
 
 @org.junit.jupiter.api.Test
@@ -24,4 +24,15 @@ class tests {
     floor.new_flr_ava(24,D1);
     a=floor.getFloorAvailability(D2);
     assertEquals(24,a);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void floortest2() {
+        data D1=new data("hi",1234);
+        D1.set_floor_vacancy(1,23);
+        floor floor=new floor("hi",1234,4);
+        int a = floor.getFloorAvailability(D1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            floor.getFloorAvailability(D1);
+        }, "Expected getFloorAvailability to throw IllegalArgumentException for non-existent floor.");
     }
