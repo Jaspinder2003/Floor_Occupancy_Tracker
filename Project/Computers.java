9a631bbfe22c3ecd74cbffa121a15d7058db0970
jaspinder singh maan <jaspindersingh.maan@ucalgary.ca>
2024-03-22 06:09:45 +0000

    changed computer

--- Project/Computers.java
+++ Project/Computers.java
@@ -5,23 +5,24 @@ class Computers extends data{
     private int computers_availability;
     private String name;
     private int id;
+    private int floor;
     public Computers(String name,int id,int floor){
         super(name,id);
         this.name=name;
         this.id=id;
-
+        this.floor=floor;
     }
 
-    public int getComputerAvailability(Integer floor,data data){
-        HashMap<Integer,Integer> m =data.getFloor_vacancy();
-        if (!m.containsKey(floor)) {
+    public int getComputerAvailability(data data){
+        HashMap<Integer,Integer> m =data.getComputer_vacancy();
+        if (!m.containsKey(this.floor)) {
             throw new IllegalArgumentException("Floor does not exist.");
         }
-        this.computers_availability=m.get(floor);
+        this.computers_availability=m.get(this.floor);
         return computers_availability;
     }
-    public void new_computer_ava(Integer floor,int computer_ava,data data){
-        data.set_computer_vacancy(floor,computer_ava);
+    public void new_computer_ava(int computer_ava,data data){
+        data.set_computer_vacancy(this.floor,computer_ava);
     }
 
 
