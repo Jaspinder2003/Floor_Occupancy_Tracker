3daa64bc0bea480cf9515bd19333700c72d29b12
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-03-22 09:10:53 +0000

    added hashset and getfloor method

--- Project/Floor.java
+++ Project/Floor.java
@@ -1,25 +1,35 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 class floor extends data{
     private int floor_availability;
     private String name;
     private int id;
     private int floor;
+
+    private static HashSet<floor> floorObjects = new HashSet<floor>(); 
+
+
     public floor(String name,int id,int floor){
         super(name,id);
         this.floor=floor;
     }
 
-    public int getFloorAvailability(data data){
+    public Integer getFloor(){
+        return floor;
+    }
+
+    public static void AddUser(floor floor) {
+        floorObjects.add(floor);
+    }
+
+    public int getFloorAvailability(Integer floor,data data){
         HashMap<Integer,Integer> m =data.getFloor_vacancy();
-        if (!m.containsKey(floor)) {
-            throw new IllegalArgumentException("Floor does not exist.");
-        }
-        this.floor_availability=m.get(this.floor);
+        this.floor_availability=m.get(floor);
         return floor_availability;
     }
-    public void new_flr_ava(int flr_ava,data data){
-        data.set_floor_vacancy(this.floor,flr_ava);
+    public void new_flr_ava(Integer floor,int flr_ava,data data){
+        data.set_floor_vacancy(floor,flr_ava);
     }
 
 
