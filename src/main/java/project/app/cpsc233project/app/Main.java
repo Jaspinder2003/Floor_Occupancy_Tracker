20cf64ba9223261addf158eaffded9ec48e69d91
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-04-11 21:54:38 +0000

    fixed FXML file path in start method

--- src/main/java/project/app/cpsc233project/app/Main.java
+++ src/main/java/project/app/cpsc233project/app/Main.java
@@ -11,9 +11,11 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/project/app/cpsc233project/fxml/Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("TFDL Occupency Tracker!");
+        MainController controller = fxmlLoader.getController();
+        controller.setStage(stage);
         stage.setScene(scene);
         stage.show();
     }
