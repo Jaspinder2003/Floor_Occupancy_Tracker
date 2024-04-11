af23fd1e11b34777dd26e3a768674d113e3365ae
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-04-11 22:10:24 +0000

    added link sign in and sign out fxmls

--- src/main/java/project/app/cpsc233project/app/MainController.java
+++ src/main/java/project/app/cpsc233project/app/MainController.java
@@ -1,9 +1,17 @@
 package project.app.cpsc233project.app;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -11,6 +19,43 @@ public class MainController {
 
     @FXML
     private Button welcomePageSignIn;
+
+    private Stage stage;
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    private void SignInButton(ActionEvent event) {
+        try {
+
+            Parent signInParent = FXMLLoader.load(getClass().getResource("/project/app/cpsc233project/fxml/SignIn.fxml"));
+            Scene signInScene = new Scene(signInParent);
+
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(signInScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception here (maybe show an error dialog)
+        }
+    }
+
+    @FXML
+    private void SignOutButton(ActionEvent event) {
+        try {
+            Parent signOutParent = FXMLLoader.load(getClass().getResource("/project/app/cpsc233project/fxml/SignOut.fxml"));
+            Scene signOutScene = new Scene(signOutParent);
+
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(signOutScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception here
+        }
+    }
+
 
 
 
