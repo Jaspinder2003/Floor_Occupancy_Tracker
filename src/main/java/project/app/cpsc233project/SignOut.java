df6a31b8747c5eff0cba3d2b618ced028648847d
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-04-12 09:03:39 +0000

    updated signout

--- src/main/java/project/app/cpsc233project/SignOut.java
+++ src/main/java/project/app/cpsc233project/SignOut.java
@@ -1,32 +1,44 @@
 package project.app.cpsc233project;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class SignOut extends Menu {
 
-    @Override
-    public void execute(String name, int studentID, boolean ComputerUsage) {
-        System.out.println("Please enter your UCID to sign out:");
-        int ucid = getIntegerInput();
+    public void execute(String name, int studentID, boolean computerUsage, String floorN, String area){}
+
+    public void executeS(int ucid) {
+
+
 
         data userFloor = findUser(ucid);
+
         Computers c=findComp(ucid);
+
         if (userFloor != null) {
             data.dataObjects.remove(userFloor); // Remove the user's floor from the tracking set
-            floor userFloor1 = new floor(userFloor.getName(), userFloor.getID(), userFloor.getfloor());
+            floor.floorObjects.remove(userFloor);
+            floor userFloor1 = new floor(userFloor.getName(), userFloor.getID(), userFloor.getfloor(), userFloor.getarea());
             int currentFloorAvailability = userFloor1.getFloorAvailability(userFloor.getfloor(), userFloor);
             userFloor1.new_flr_ava(userFloor.getfloor(), currentFloorAvailability + 1, userFloor);
+
+
+
 
             if(c!=null){
                 Computers.ComputerObjects.remove(c);
                 int currentComputerAvailability = c.getComputerAvailability(c.getFloor(), userFloor);
                 c.new_computer_ava(c.getFloor(), currentComputerAvailability + 1, userFloor);
+
+
             }
-            System.out.println("You have successfully signed out. Thank you!");
-            
+
+            clearFile("C:/Users/Randh/Desktop/cpsc-233-group-proeject-w24-master/ProjectDB.csv");
+
             // Further logic to update floor and computer availability can be added here
 
-        } else {
-            System.out.println("UCID not found in the db.");
         }
-
     }
 
     private data findUser(int ucid) {
@@ -48,4 +60,15 @@ public class SignOut extends Menu {
         }
         return null; // Return null if the user's floor object is not found
     }
+
+    public static void clearFile(String fileName) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            // File is automatically cleared by opening it in overwrite mode
+        } catch (IOException e) {
+            System.err.println("Failed to clear the file: " + fileName);
+            e.printStackTrace();
+        }
+    }
+
+
 }
