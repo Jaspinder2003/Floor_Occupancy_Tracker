3ca1d2842b134497cd8d930872ff91dfd92bc963
Yadwinder Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-04-14 05:14:33 +0000

    Added method to return to home screen

--- src/main/java/project/app/cpsc233project/app/statsController.java
+++ src/main/java/project/app/cpsc233project/app/statsController.java
@@ -1,9 +1,14 @@
 package project.app.cpsc233project.app;
 
+import java.net.URL;
+
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -35,7 +40,9 @@ public class statsController {
     @FXML
     private Button userList;
 
-    @SuppressWarnings("unused")
+    @FXML
+    private Button returnHome;
+
     private Stage stage;
     @SuppressWarnings("exports")
     public void setStage(Stage stage) {
@@ -189,6 +196,21 @@ public class statsController {
             popUpStage.setScene(scene);
             popUpStage.showAndWait();
         }
+    }
 
+    public void returnToHomepage() {
+        Platform.runLater(() -> {
+            try {
+                URL url = getClass().getResource("/project/app/cpsc233project/fxml/Main.fxml");
+                FXMLLoader loader = new FXMLLoader(url);
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) statsChoice.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
