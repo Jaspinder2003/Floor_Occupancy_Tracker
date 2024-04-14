5e98d5a24ddca41627e018f0743f7c2eef3002ad
Navpreet Singh <navpreet.singh3@ucalgary.ca>
2024-04-14 06:15:25 +0000

    pasted incorrect update in last commit, fixed that

--- src/main/java/project/app/cpsc233project/app/Main.java
+++ src/main/java/project/app/cpsc233project/app/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/project/app/cpsc233project/fxml/Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 640,600);
         stage.setTitle("TFDL Occupency Tracker!");
         MainController controller = fxmlLoader.getController();
         controller.setStage(stage);
