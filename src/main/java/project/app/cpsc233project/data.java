d22444c80589868b8684f7f990255fee9b06a21f
Yadwinder Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-04-16 03:00:18 +0000

    Update data.java

--- src/main/java/project/app/cpsc233project/data.java
+++ src/main/java/project/app/cpsc233project/data.java
@@ -1,3 +1,13 @@
+
+/**
+ * 
+ * @author Yadwinder Singh Dhaliwal @tutorial T05
+ * @author Jaspinder Singh Maan @tutorial T15
+ * @author Navpreet Singh @tutorial T08
+ * @created 2024-04-15
+ * 
+ */
+ 
 package project.app.cpsc233project;
 import java.util.ArrayList;
 import java.util.Arrays;
