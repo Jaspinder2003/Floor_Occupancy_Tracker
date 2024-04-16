bd10c23270660a0f0a2ce89783b41d7f4a76c8df
Yadwinder Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-04-16 03:00:30 +0000

    Update dataidcomparitor.java

--- src/main/java/project/app/cpsc233project/dataidcomparitor.java
+++ src/main/java/project/app/cpsc233project/dataidcomparitor.java
@@ -1,5 +1,14 @@
 package project.app.cpsc233project;
 
+/**
+ * 
+ * @author Yadwinder Singh Dhaliwal @tutorial T05
+ * @author Jaspinder Singh Maan @tutorial T15
+ * @author Navpreet Singh @tutorial T08
+ * @created 2024-04-15
+ * 
+ */
+
 import java.util.Comparator;
 
 class dataidcomparator implements Comparator<data> {
