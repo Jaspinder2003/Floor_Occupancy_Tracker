f46703672bc5ac3b33ab1a73a8229c63d33afc23
Yadwinder Dhaliwal <yadwinder.dhaliwal@ucalgary.ca>
2024-04-16 02:59:28 +0000

    Update Menu.java

--- src/main/java/project/app/cpsc233project/Menu.java
+++ src/main/java/project/app/cpsc233project/Menu.java
@@ -1,6 +1,15 @@
 package project.app.cpsc233project;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Yadwinder Singh Dhaliwal @tutorial T05
+ * @author Jaspinder Singh Maan @tutorial T15
+ * @author Navpreet Singh @tutorial T08
+ * @created 2024-04-15
+ * 
+ */
+
 public abstract class Menu {
     protected Scanner scanner;
     
